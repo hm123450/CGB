21057ddd66127e87a5e67b054221629469b51687
hm123450 <149764448+hm123450@users.noreply.github.com>
2025-10-03 18:35:04 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,85 @@
-<script setup>
-// 如果页面不需要导航，可以不导入 element-plus 的 Icon
+<script lang="ts" setup>
+
+import { ElIcon } from 'element-plus'
+import { SuccessFilled } from '@element-plus/icons-vue'
+
+// 项目主页
+const logo = {
+    name: "Project",
+    link: "#",
+}
+
+
 </script>
 
 <template>
-  <div>
-    <!-- 这里放正文，比如标题、作者、摘要、视频等 -->
-    <h1>ChangingGrounding: 3D Visual Grounding in Changing Scenes</h1>
-    <p>
-      Real-world robots localize objects from natural-language instructions while scenes around them keep changing...
-    </p>
-  </div>
+  <!-- 设置页首始终悬浮顶部 -->
+  <el-affix :offset="0" :style="{width: '100%'}">
+    <!-- 水平导航目录 -->
+    <el-menu 
+        default-active="1"
+        class="el-menu-demo"
+        mode="horizontal"
+        :ellipsis="false"
+    >
+        <!-- 项目主页（唯一保留的） -->
+        <el-menu-item index="1">
+            <el-icon :size="20"><SuccessFilled /></el-icon>
+            <a :href="logo.link">{{ logo.name }}</a>
+        </el-menu-item>
+    </el-menu>
+  </el-affix>
 </template>
 
+
+<style>
+
+/* 导航背景 */
+.el-menu {
+	background: none;
+}
+
+/* 下拉菜单宽度 */
+.el-menu--popup {
+	min-width: 100%;
+}
+
+/* 导航背景特效和阴影 */
+.el-affix--fixed {
+	box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+	background-image: radial-gradient(#ffffffca 1px,#ffffff 1px);
+	background-size: 3px 3px;
+}
+
+/* 水平导航高度 */
+.el-header {
+    --el-header-height: 50px;
+}
+
+</style>
+
 <style scoped>
-/* 页面正文样式，可以按需调整 */
-h1 {
-  font-size: 42px;
-  text-align: center;
-  margin: 32px 0;
+
+/* 水平导航左右分区 */
+.el-menu--horizontal > .el-menu-item:nth-child(2) {
+    margin-right: auto;
 }
-p {
-  font-size: 18px;
-  line-height: 1.6;
-  text-align: justify;
-  margin: 0 auto;
-  width: 70%;
+
+/* 水平导航高度 */
+.el-menu--horizontal {
+    --el-menu-horizontal-height: 50px;
 }
+
+/* 取消鼠标焦点悬浮在链接上的颜色装饰 */
+a:hover {
+  color: inherit;
+  border-bottom: none;
+}
+
+/* 链接装饰，取消下划线和链接颜色 */
+a {
+	text-decoration: None;
+	color: inherit;
+}
+
 </style>
