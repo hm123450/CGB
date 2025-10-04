e304cdc5afc3c5267800004477249e3634c854aa
hm123450 <149764448+hm123450@users.noreply.github.com>
2025-10-04 10:44:18 +0800

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,24 +5,12 @@ import Markdown from './sections/Markdown.vue'
 import BibTeX from './sections/BibTeX.vue'
 import Carousel from './sections/Carousel.vue'
 import Video from './sections/Video.vue'
-import ImageSlider from './sections/ImageSlider.vue'
-import ImageSliderInner from './sections/ImageSliderInner.vue'
-import ImageSelector from './sections/ImageSelector.vue'
-import Echart from './sections/Echart.vue'
-import Table from './sections/Table.vue'
-import Collapse from './sections/Collapse.vue'
-import SeletionForComparison from './sections/SeletionForComparison.vue'
-import Comment from './sections/Comment.vue'
-import Latex from './sections/Latex.vue'
-import Model3D from './sections/Model3D.vue'
-import GaussianSplats3D from './sections/GaussianSplats3D.vue'
 
 // benchmark statics method experiment demo
 
 export default {
   components: {
     Title,
-    Carousel,
     Video,
     Abstract,
     Markdown,
@@ -33,7 +21,6 @@ export default {
 
 <template>
   <Title/>
-  <Carousel/>
   <Video/>
   <Abstract/>
   <Markdown/>
